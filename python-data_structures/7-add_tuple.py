778b6d0781f291ab758da9d408145cb9132566d9
seyyar95 <heydrovsyyar@gmail.com>
2024-01-25 09:15:57 -0800

    tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-def check_tuple(tuple_n=()):
-    if len(tuple_n) == 0:
-        return 0, 0
-    elif len(tuple_n) == 1:
-        return tuple_n[0], 0
-    else:
-        return tuple_n
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_1 = check_tuple(tuple_a)
-    tuple_2 = check_tuple(tuple_b)
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
     return tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
