be8e359f2e3c79241dcca2eac1c9f4a1171fe659
seyyar95 <heydrovsyyar@gmail.com>
2024-01-27 01:22:49 -0800

    alphabets are mad

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i -32), end="")
+    if i % 2 != 0:
+        i -= 32
+    print(chr(i), end="")
