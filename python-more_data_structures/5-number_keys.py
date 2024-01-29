7fe9872aeaac7f5598646478638eba51c0cef87b
seyyar95 <heydrovsyyar@gmail.com>
2024-01-29 05:56:42 -0800

    Number of keys

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    '''
+    we can do it like that
+
+    x = 0
+    for i in a_dictionary:
+        x = x + 1
+    return x
+
+    but there's an easy way 
+    '''
+    return len(a_dictionary)
