724123f1c9169b0a1789ecca4a405be6d65c232c
seyyar95 <heydrovsyyar@gmail.com>
2024-01-30 22:04:39 -0800

    don't use reversed func:)

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, int):
         return 0
 
+    num = 0
+
     for i in range(len(roman_string)):
         if i > 0 and rom_dic[roman_string[i]] > rom_dic[roman_string[i - 1]]:
             num = num + rom_dic[roman_string[i]] - 2
