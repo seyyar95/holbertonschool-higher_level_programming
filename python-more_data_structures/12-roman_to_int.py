1f6ba6baf6ac2ee337b485eb7d5458c251c8fd47
seyyar95 <heydrovsyyar@gmail.com>
2024-01-30 22:05:57 -0800

    don't use reversed func:)

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, int):
         return 0
 
+    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
     num = 0
 
     for i in range(len(roman_string)):
