3de8c4f57da86a26ac36639d13f1a346fc8d9ca2
seyyar95 <heydrovsyyar@gmail.com>
2024-01-30 22:02:09 -0800

    don't use reversed func:)

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
+    if roman_string is None or isinstance(roman_string, int):
         return 0
-    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    """
-    this one also works
 
     for i in range(len(roman_string)):
         if i > 0 and rom_dic[roman_string[i]] > rom_dic[roman_string[i - 1]]:
             num = num + rom_dic[roman_string[i]] - 2
-            # * rom_dic[roman_string[i - 1]]
         else:
             num = num + rom_dic[roman_string[i]]
-
-       but we have this too
-    """
-    element = 0
-    for i in reversed(roman_string):
-        val = rom_dic[i]
-        if val >= element:
-            num = num + val
-        else:
-            num = num - val
-        element = val
     return num
