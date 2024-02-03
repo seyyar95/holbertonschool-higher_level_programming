a7266dab9e929f614e4f11d382e87b5e64fa3cd5
seyyar95 <heydrovsyyar@gmail.com>
2024-02-03 08:38:58 -0800

    "The only thing we have to fear is fear itself."

---
+++ python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Represents a square shape'''
+
+
+class Square:
+    '''Documantation for square class'''
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
