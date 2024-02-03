6ae04bd4bc130d4aa69fecad0e60ae5dd7ca1e24
seyyar95 <heydrovsyyar@gmail.com>
2024-02-03 10:33:11 -0800

    such a messy task

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,6 +13,10 @@ class Square:
     def size(self):
         return self.__size
 
+    @property
+    def position(self):
+        return self.__position
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -20,10 +24,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
-    def position(self):
-        return self.__position
 
     @position.setter
     def position(self, value):
