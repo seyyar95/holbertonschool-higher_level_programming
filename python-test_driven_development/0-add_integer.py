dfe29a2678ab0f9200355a965f71910fd45623a9
seyyar95 <heydrovsyyar@gmail.com>
2024-02-04 09:22:54 -0800

    life is hard, and doctest makes it harder

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    """ A function that adds 2 integers together and returns their sum
+"""This module contains add_integer function"""
 
-    >>> add_integer(2, 3)
-    5
-    >>> add_integer(1, -1)
-    0
+def add_integer(a, b=98):
+    """a function that adds 2 integers together and returns their sum
+    Args:
+        a: the first int
+        b: the second int, default 98
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
