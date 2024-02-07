fc4b5c3d98b492f43ab2fe626732e0c3d1614901
seyyar95 <heydrovsyyar@gmail.com>
2024-02-07 05:45:21 -0800

    my checker is different

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-"""a function that returns the list of available attributes and methods of an object"""
+"""Module doc"""
+
+
 def lookup(obj):
+    """a function that returns the list of available
+    attributes and methods of an object"""
+
     return dir(obj)
