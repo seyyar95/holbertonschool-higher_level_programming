83d48302e6931823b524b6930e27c91281b91386
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 02:13:59 -0800

    Better call base

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Geometry class."""
+
+
+class BaseGeometry:
+    """
+    Represents a  Geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
