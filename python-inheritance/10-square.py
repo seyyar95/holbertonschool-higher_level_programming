5be16e5fc14fb9b3a2528171bb794c2feee8c215
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 06:22:27 -0800

    In three words I can sum up everything

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,7 +5,7 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square:
+class Square(Rectangle):
     """Represents a square"""
     def __init__(self, size):
         self.integer_validator("size", size)
