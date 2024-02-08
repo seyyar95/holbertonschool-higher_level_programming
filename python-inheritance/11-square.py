8c77db458981e34d3b0227904da943fca10f5415
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 06:37:37 -0800

    special commit

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module doc"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        super().__str__()
