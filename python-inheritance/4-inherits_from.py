ecb5c8cf83c03dabccfb101e86c1e09f301088fd
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 02:02:48 -0800

    confused

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Doc for module """
+
+
+def inherits_from(obj, a_class):
+    """a function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from
+    the specified class ; otherwise False."""
+    
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
