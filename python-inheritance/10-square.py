271f018c13a99ec9e55674ca83f90b21cc5d7fdf
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 06:08:21 -0800

    Isn't it right?

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module doc"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
