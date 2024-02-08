b62dd3796021823e1c660bde6bcd46726b03e9d0
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 00:46:32 -0800

    Dresser des listes est l'une des plus anciennes activités du poète.

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,6 +3,6 @@
 
 
 def is_same_class(obj, a_class):
-   """a function that returns True if the object is exactly
-   an instance of the specified class, otherwise False"""
-   return type(obj) is a_class
+    """a function that returns True if the object is exactly
+    an instance of the specified class, otherwise False"""
+    return type(obj) is a_class
