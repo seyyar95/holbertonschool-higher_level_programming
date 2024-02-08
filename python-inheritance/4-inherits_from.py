e769d114cd3520aae90cd12ad9d41f9a557a2bd5
seyyar95 <heydrovsyyar@gmail.com>
2024-02-08 02:05:45 -0800

    confused

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,6 +7,6 @@ def inherits_from(obj, a_class):
     that inherited (directly or indirectly) from
     the specified class ; otherwise False."""
 
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     return False
