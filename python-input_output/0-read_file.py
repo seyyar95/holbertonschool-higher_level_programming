bdb2f1761797d8a2aa770cd6411ff2422fabbec8
seyyar95 <heydrovsyyar@gmail.com>
2024-02-10 01:05:52 -0800

    trying to do something

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Module doc"""
+
+
 def read_file(filename=""):
+    """function doc"""
     with open(filename, "r", encoding="utf-8") as f:
         text = f.read()
         print(text)
