18b44e79c015de1586e633952c047c614fdb5a4a
seyyar95 <heydrovsyyar@gmail.com>
2024-02-10 01:30:03 -0800

    everything is okay

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """function doc"""
     with open(filename, "r", encoding="utf-8") as f:
-        text = f.read()
-        print(text)
+        print(f.read(), end="")
