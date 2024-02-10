48e3e3bc3170c4fe5ffc67d953224b7024052a12
seyyar95 <heydrovsyyar@gmail.com>
2024-02-10 08:56:36 -0800

    What's happening?

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module doc"""
+
+
+class Student:
+    """a class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list)
+           and all(isinstance(item, str) for item in attrs)):
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
