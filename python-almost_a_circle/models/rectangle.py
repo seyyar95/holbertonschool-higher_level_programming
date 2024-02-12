2475d649a888fdc7ef752f2eaa3877afe46fb599
seyyar95 <heydrovsyyar@gmail.com>
2024-02-12 00:29:18 -0800

    Integer validator added

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        if type(width) is not int:
+            raise TypeError(f"width must be an integer")
+        if width <= 0:
+            raise ValueError(f"width must be > 0")
         self.__width = width
 
     @property
@@ -28,6 +32,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        if type(height) is not int:
+            raise TypeError(f"height must be an integer")
+        if height <= 0:
+            raise ValueError(f"height must be > 0")
         self.__height = height
 
     @property
@@ -36,6 +44,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        if type(x) is not int:
+            raise TypeError(f"x must be an integer")
+        if x < 0:
+            raise ValueError(f"x must be >= 0")
         self.__x = x
 
     @property
@@ -44,4 +56,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        if type(y) is not int:
+            raise TypeError(f"y must be an integer")
+        if y < 0:
+            raise ValueError(f"y must be >= 0")
         self.__y = y
