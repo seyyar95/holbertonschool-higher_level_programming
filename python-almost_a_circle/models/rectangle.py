af9a4903926f6549dd4a96ab3c3558f7c8223e7c
seyyar95 <147692152+seyyar95@users.noreply.github.com>
2024-02-12 14:28:04 +0400

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,7 +71,7 @@ class Rectangle(Base):
         """public method that prints
         stdout the rectangle instance"""
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """__str__ method"""
