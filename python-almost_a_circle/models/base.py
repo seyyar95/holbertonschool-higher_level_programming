3ecb1de32cb9c7065ebf54768bfbce33465b38de
seyyar95 <heydrovsyyar@gmail.com>
2024-02-11 22:28:27 -0800

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Base class module"""
+
+
+class Base:
+    """ Base class providing a unique
+    identifier for its instances
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is  None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
