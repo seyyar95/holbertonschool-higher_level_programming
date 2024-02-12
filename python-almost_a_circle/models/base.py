48c4329a14fe229a492de9edd70b38324956c513
seyyar95 <heydrovsyyar@gmail.com>
2024-02-11 22:30:10 -0800

    Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Base class module"""
 
 
@@ -11,7 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is  None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
