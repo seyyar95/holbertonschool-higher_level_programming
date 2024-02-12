e4d003ff6e8a90f00340342b6c0041b6fff9160a
seyyar95 <heydrovsyyar@gmail.com>
2024-02-12 04:14:30 -0800

    added kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,16 +84,29 @@ class Rectangle(Base):
 
     def update(self, *args):
         """doc of update"""
-        a = 0
-        for arg in args:
-            if a == 0:
-                self.id = arg
-            elif a == 1:
-                self.width = arg
-            elif a == 2:
-                self.height = arg
-            elif a == 3:
-                self.x = arg
-            elif a == 4:
-                self.y = arg
-            a += 1
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "id":
+                    self.id = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
