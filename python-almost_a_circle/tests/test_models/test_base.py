8d109c2e9eca8ad1f0bbfee2e44f0f362738087d
seyyar95 <heydrovsyyar@gmail.com>
2024-02-13 07:36:25 -0800

    Trying to write some test cases

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,8 +7,13 @@ from models.base import Base
 
 class BaseTest(unittest.TestCase):
     def test_id(self):
+
         base1 = Base()
         self.assertEqual(base1.id, 1)
+
+        base2 = Base(None)
+        self.assertEqual(base1.id, 2)
+
 
 
 if __name__ == "__main__":
