fd87cc12b218933466376d37cabd8f159dc4494e
seyyar95 <heydrovsyyar@gmail.com>
2024-02-13 08:14:26 -0800

    Trying to write some tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,9 +7,29 @@ from models.base import Base
 
 class BaseTest(unittest.TestCase):
     def test_id(self):
+        # None case
 
         base1 = Base()
         self.assertEqual(base1.id, 1)
 
         base1 = Base(None)
         self.assertEqual(base1.id, 2)
+
+        # positive int case
+        base1 = Base(10)
+        self.assertEqual(base1.id, 10)
+
+        base1 = Base(100)
+        self.assertEqual(base1.id, 100)
+
+        # negative int case
+        base1 = Base(-1)
+        self.assertEqual(base1.id, -1)
+
+        base1 = Base(-25)
+        self.assertEqual(base1.id, -25)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
