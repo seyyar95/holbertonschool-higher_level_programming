07e0400f906125bd054ee76265ef0b51b6d407e0
seyyar95 <heydrovsyyar@gmail.com>
2024-02-14 03:46:12 -0800

    Base test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,15 +6,35 @@ import unittest
 from models.base import Base
 
 class BaseTest(unittest.TestCase):
-    def test_id(self):
-        base1 = Base()
-        self.assertEqual(base1.id, 1)
+    def test_id_checker(self):
+        # None case
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-        base2 = Base(None)
-        self.assertEqual(base2.id, 2)
+        b1 = Base(None)
+        self.assertEqual(b1.id, 2)
 
-        base3 = Base(89)
-        self.assertEqual(base3.id, 3)
+        # Positive integer case
+        b2 = Base(3)
+        self.assertEqual(b2.id, 3)
+
+        b2 = Base(4)
+        self.assertEqual(b2.id, 4)
+
+        # Negative integer case
+        b3 = Base(-5)
+        self.assertEqual(b3.id, -5)
+
+        b3 = Base(-6)
+        self.assertEqual(b3.id, -6)
+
+        # String case
+        b4  = Base("Saul Goodman was here.")
+        self.assertEqual(b4.id, "Saul Goodman was here.")
+
+        b4 = Base("If you don't like Kanye, I don't like you.")
+        self.assertEqual(b4.id, "If you don't like Kanye, I don't like you.")
+
 
 
 if __name__ == "__main__":
