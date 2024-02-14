88aa9e94266e90c9a1be407f63a9a274277984c1
seyyar95 <heydrovsyyar@gmail.com>
2024-02-13 22:21:09 -0800

    Still trying

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,11 +7,11 @@ from models.base import Base
 
 class BaseTest(unittest.TestCase):
     def test_id(self):
-        # None case
-
+        
         base1 = Base()
         self.assertEqual(base1.id, 1)
-
+        
+        # none case
         base1 = Base(None)
         self.assertEqual(base1.id, 2)
 
