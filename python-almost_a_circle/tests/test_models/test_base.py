e339923fcbb28dfd558714b4c851a23fc245441b
seyyar95 <heydrovsyyar@gmail.com>
2024-02-14 03:58:37 -0800

    Base test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,35 +6,29 @@ import unittest
 from models.base import Base
 
 
-class BaseTest(unittest.TestCase):
-    def test_id_checker(self):
-        # None case
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+class TestBase(unittest.TestCase):
 
-        b1 = Base(None)
-        self.assertEqual(b1.id, 2)
+    def test_id_none(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
 
-        # Positive integer case
-        b2 = Base(3)
-        self.assertEqual(b2.id, 3)
+    def test_id_positive(self):
+        b = Base(3)
+        self.assertEqual(b.id, 3)
 
-        b2 = Base(4)
-        self.assertEqual(b2.id, 4)
+    def test_id_negative(self):
+        b = Base(-3)
+        self.assertEqual(b.id, -3)
 
-        # Negative integer case
-        b3 = Base(-5)
-        self.assertEqual(b3.id, -5)
+    def test_to_json_string(self):
+        self.assertEqual(Base.to_json_string(None), '[]')
+        self.assertEqual(Base.to_json_string([]), '[]')
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
 
-        b3 = Base(-6)
-        self.assertEqual(b3.id, -6)
-
-        # String case
-        b4 = Base("Saul Goodman was here.")
-        self.assertEqual(b4.id, "Saul Goodman was here.")
-
-        b4 = Base("If you don't like Kanye, I don't like you.")
-        self.assertEqual(b4.id, "If you don't like Kanye, I don't like you.")
+    def test_from_json_string(self):
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string('[]'), [])
+        self.assertEqual(Base.from_json_string('[{ "id": 89}]'), [{"id": 89}])
 
 
 if __name__ == "__main__":
