30f266eca73c6e695cb360732c2e7a717174fbd7
seyyar95 <heydrovsyyar@gmail.com>
2024-02-14 22:19:05 -0800

    Rectangle tests made me mad

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,6 +7,7 @@ import unittest
 from models.rectangle import Rectangle
 from io import StringIO
 from unittest.mock import patch
+import os
 
 
 class TestRectangle(unittest.TestCase):
@@ -98,3 +99,59 @@ class TestRectangle(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as out:
             self.r.display()
             self.assertEqual(out.getvalue(), output)
+
+    def test_create(self):
+        dictionary = {"height": 2, "width": 1, "x": 3, "y": 4, "id": 89}
+        r3 = Rectangle.create(**dictionary)
+        self.assertEqual(str(r3), "[Rectangle] (89) 3/4 - 1/2")
+
+    def test_save_to_file(self):
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), "[]")
+
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+
+        Rectangle.save_to_file([Rectangle(1, 2, id=13)])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(
+                file.read(), '[{"id": 13, "width": 1, "height": 2, "x": 0, "y": 0}]')
+
+    def test_load_from_file1(self):
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+
+        self.assertEqual(Rectangle.load_from_file(), [])
+
+    def test_load_from_file_2(self):
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+
+        r1 = Rectangle(5, 5)
+
+        inp = [r1]
+        Rectangle.save_to_file(inp)
+        out = Rectangle.load_from_file()
+
+        self.assertEqual(inp[0].__str__(), out[0].__str__())
