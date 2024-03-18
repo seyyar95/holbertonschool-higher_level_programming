644c761a17d9dbedcf5c8e647c67007dac97e069
seyyar95 <heydrovsyyar@gmail.com>
2024-03-18 15:45:24 +0000

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (state_id), REFERENCES (states.id));
+	FOREIGN KEY (state_id) REFERENCES (states.id));
