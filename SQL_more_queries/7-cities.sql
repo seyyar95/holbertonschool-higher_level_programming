87076820c785142bcedd5b4da924695be27d537c
seyyar95 <heydrovsyyar@gmail.com>
2024-03-18 15:38:33 +0000

    Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- use database
+USE hbtn_0d_usa;
+
+-- create table citites
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id), REFERENCES (states.id));
