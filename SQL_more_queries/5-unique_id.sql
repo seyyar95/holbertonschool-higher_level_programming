d78579066896bb8389cb279e404c5e468d3fae0b
Səyyar Heydərov <heydrovsyyar@gmail.com>
2024-03-20 17:18:01 +0400

    Checker doesn't check everything

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creates the table unique_id on MySQL server.
-CREATE TABLE unique_id (id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
