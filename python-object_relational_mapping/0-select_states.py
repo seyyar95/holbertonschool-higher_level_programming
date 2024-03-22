3e314bd9bfc8189851a8c9d10dceaeb59b1aa3ca
seyyar95 <heydrovsyyar@gmail.com>
2024-03-22 12:54:35 +0000

    For ABil

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,48 +1,27 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
-import sys
+"""
+    The `Get all states` module
+"""
 import MySQLdb
-
-
-def connect_to_database():
-    """Establishes a connection to the MySQL database."""
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-            charset='utf8'
-        )
-        return db
-    except MySQLdb.Error as e:
-        print(f"Error {e.args[0]}: {e.args[1]}")
-        sys.exit(1)
-
-
-def fetch_and_print_data(db):
-    """Fetches data from the 'states' table and prints it."""
-    try:
-        with db.cursor() as cur:
-            cur.execute("SELECT * FROM states ORDER BY id ASC")
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
-    except MySQLdb.Error as e:
-        print(f"Error {e.args[0]}: {e.args[1]}")
-
-
-def main():
-    """Main function to execute the script."""
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-
-    db = connect_to_database()
-    fetch_and_print_data(db)
-    db.close()
-
+import sqlalchemy
+import sys
 
 if __name__ == "__main__":
-    main()
+    argv = sys.argv
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=user,
+                           passwd=passwd,
+                           db=db,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
