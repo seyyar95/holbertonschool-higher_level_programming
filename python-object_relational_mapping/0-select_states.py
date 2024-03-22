72f363a8a4570a92eec0613a2bbbeefd5a60f6c6
seyyar95 <heydrovsyyar@gmail.com>
2024-03-22 12:56:45 +0000

    For ABil

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """
-    The `Get all states` module
+Connecting to database and listing it
 """
-import MySQLdb
-import sqlalchemy
-import sys
 
 if __name__ == "__main__":
-    argv = sys.argv
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
+    import MySQLdb
+    from sys import argv
 
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=user,
-                           passwd=passwd,
-                           db=db,
-                           charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+    try:
+        cur.execute("""SELECT * FROM states ORDER BY id""")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(e)
+
+    for row in rows:
         print(row)
+
     cur.close()
-    conn.close()
+    db.close()
