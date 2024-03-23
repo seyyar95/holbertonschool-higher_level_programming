146f4387dc9d120f115113369711a5aeec8d97c0
seyyar95 <heydrovsyyar@gmail.com>
2024-03-23 07:25:59 +0000

    autoincrement added

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Contains the class definition of a State
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Class that defines each State
+    """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
